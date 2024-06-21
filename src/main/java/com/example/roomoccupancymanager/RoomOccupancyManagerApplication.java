991ca1bb36231355888e637a3969b7274208bca7
Krzysztof Ożarek <krzysztof.ozarek@schange.com>
2024-06-21 11:50:10 +0200

    initial commit

---
+++ src/main/java/com/example/roomoccupancymanager/RoomOccupancyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.roomoccupancymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomOccupancyManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomOccupancyManagerApplication.class, args);
+    }
+
+}
